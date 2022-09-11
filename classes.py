776b6afc038da83c0df82ed5864dcf3a0dcb8c82
Lucas-Cerdeira <cerdeiralucas4121@gmail.com>
2022-09-11 20:54:03 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -20,35 +20,6 @@ class User:
         return self._cidade
 
 
-class DataBase:
-
-    def __init__(self):
-        self.conexao = sqlite3.connect('basededados.db')
-        self.cursor = self.conexao.cursor()
-
-    def add_pessoas(self, pessoa: User):
-        self.cursor.execute('INSERT INTO pessoas Values (nome:, idade:, cidade:)',
-                            {'nome': pessoa.nome, 'idade': pessoa.idade, 'cidade': pessoa.cidade})
-
-        self.conexao.commit()
-
-        self.cursor.close()
-        self.conexao.close()
-
-    def ver_pessoas(self):
-        self.conexao.commit()
-
-        self.cursor.execute('SELECT * FROM pessoas')
-        pessoas = []
-        for linha in self.cursor.fetchall():
-            pessoas.append(linha)
-
-        self.cursor.close()
-        self.conexao.close()
-
-        return pessoas
-
-
 
 
 
