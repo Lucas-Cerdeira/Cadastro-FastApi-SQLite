5bb6693151a80aa6372f6e29dedcb3dbb2f0d4c3
Lucas-Cerdeira <cerdeiralucas4121@gmail.com>
2022-09-11 00:27:08 -0300

    adadd

--- classes.py
+++ classes.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel
+import sqlite3
 
 
-class User():
-    def __init__(self, nome: str, idade: str):
+class User:
+    def __init__(self, nome: str, idade: str, cidade: str):
         self._nome = nome
         self._idade = idade
+        self._cidade = cidade
 
     @property
     def nome(self):
@@ -13,3 +14,29 @@ class User():
     @property
     def idade(self):
         return self._idade
+
+    @property
+    def cidade(self):
+        return self._cidade
+
+class DataBase:
+    import sqlite3
+
+    def __init__(self):
+        self.conexao = sqlite3.connect('basededados.db')
+        self.cursor = self.conexao.cursor()
+
+    def add_pessoas(self, pessoa: User):
+        self.cursor.execute('INSERT INTO pessoas Values (nome:, idade:, cidade:)',
+                            {'nome': pessoa.nome, 'idade': pessoa.idade, 'cidade': pessoa.cidade})
+
+        self.cursor.close()
+        self.conexao.close()
+
+    def ver_pessoas(self):
+        self.cursor.execute('SELECT * FROM pessoas')
+
+
+
+
+
--- main.py
+++ main.py
@@ -8,10 +8,7 @@ app = FastAPI()
 @app.post("/Pessoa")
 def add_pessoa(nome: str, idade: int):
     usuario = User(nome, idade)
-    with open('Lista_Pessoas.txt', 'a') as arquivo:
-        arquivo.write(f'{usuario.nome} - {usuario.idade}')
-        arquivo.write('\n')
-        return str(usuario)
+
 
 
 @app.get("/Pessoas")
