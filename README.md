3dea5d50a4c8be6a88a998a9772b047af85b0bf2
Lucas-Cerdeira <cerdeiralucas4121@gmail.com>
2022-09-11 12:51:49 -0300

    DONE

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Cadastro-FastApi
- 
+
+# Recebe os dados do usuário através de um end-point
+# Armazena esses dados em um banco de dados
--- main.py
+++ main.py
@@ -3,29 +3,27 @@ from classes import User
 import sqlite3
 
 
-
 app = FastAPI()
 
 conexao = sqlite3.connect('database.db', check_same_thread=False)
 cursor = conexao.cursor()
 
 
-@app.post("/Pessoa")
+@app.post("/Pessoa")  # Recebe os dados
 def add_pessoa(nome: str, idade: int, cidade: str):
     usuario = User(nome, idade, cidade)
 
     cursor.execute('INSERT INTO pessoas (nome, idade, cidade) Values (:nome, :idade, :cidade)',
-                        {'nome': usuario.nome, 'idade': usuario.idade, 'cidade': usuario.cidade})
+                   {'nome': usuario.nome, 'idade': usuario.idade, 'cidade': usuario.cidade})
     conexao.commit()
 
 
-@app.post("/Deletar pessoas")
-def deletar_pessoas(id:str):
-    cursor.execute('DELETE FROM pessoas WHERE id=:id', {'id':id})
+@app.post("/Deletar pessoas")  # Deleta
+def deletar_pessoas(id: str):
+    cursor.execute('DELETE FROM pessoas WHERE id=:id', {'id': id})
 
 
-
-@app.get("/Pessoas")
+@app.get("/Pessoas")  # Retorna uma lista com todas as informações no BD
 def ver_pessoas():
     conexao.commit()
 
